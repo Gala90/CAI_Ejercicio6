902027122da22b0ebda0eac1a1ce016c44222932
Gala <galamagdalena@hotmail.com>
2020-03-23 02:10:47 -0300

    Ejercicio6

---
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            Console.WriteLine("Ingrese un numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro numero: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            c = (a + b) * (a - b);
+
+            Console.WriteLine("Resultado: " + c);
+
+            Console.ReadKey();
+        }
+    }
+}
